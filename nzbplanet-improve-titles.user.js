28695f28a94fae6ae469ffa0a0c24cc75b96a477
Todd Sayre <captain.friendly@gmail.com>
2022-07-22 09:49:13 -0400

    Embiggen icon

--- nzbplanet-improve-titles.user.js
+++ nzbplanet-improve-titles.user.js
@@ -1,18 +1,22 @@
 // ==UserScript==
-// @name        NzbPlanet - Improve Titles
-// @namespace   https://github.com/Sporkyy/
-// @match       https://nzbplanet.net/*
-// @run-at      document-idle
-// @grant       none
-// @version     1.2
-// @author      Sporkyy
-// @description Gives the pages on NzbPlanet.net meaningful titles
-// @icon        https://www.google.com/s2/favicons?domain=nzbplanet.net
-// @noframes
+// @name         NzbPlanet - Improve Titles
+// @namespace    https://github.com/Sporkyy/
+// @version      1.0.3
+// @description  Gives the pages on NzbPlanet.net meaningful titles
+// @author       Sporkyy
+// @match        https://nzbplanet.net/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=nzbplanet.net
+// @grant        none
 // ==/UserScript==
 
 (() => {
-  const h1 = document.querySelector('h1');
-  const wdt = window.document.title;
-  window.document.title = h1?.textContent?.trim() ?? wdt;
+  'use strict';
+
+  const qs = (s, c = document) => c.querySelector(s);
+
+  const h1 = qs('h1');
+  const wd = window.document;
+  const wdt = wd.title;
+
+  wd.title = h1?.textContent?.trim() ?? wdt;
 })();
